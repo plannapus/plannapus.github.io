af047e64d9db67891beba91cc423cdad171e3d89
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2020-11-26 12:45:24 +0100

    modified twitter timeline again (got rid of header)

--- static/twitter.js
+++ static/twitter.js
@@ -6,10 +6,11 @@ customizeTweetMedia();
 var customizeTweetMedia = function() {
 
 //overrides font styles and removes the profile picture and media from twitter feed
-jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display', 'none');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('img.Avatar').css('display', 'none');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-avatar.Identity-avatar').remove();
-jQuery('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-size', '16px');
+jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-text').css('font-size', '16px');
+jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Header').css('display', 'none');
+jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('margin-left', '1px');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-author').css('padding-left', '1px');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-text').css('margin-left', '1px');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-text').css('margin-bottom', '1px');
@@ -19,8 +20,7 @@ jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Twe
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-actions').css('margin-left', '1px');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList-tweet').css('border-top','1px solid rgb(222,122,0)')
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList-tweet:first-of-type').css('border-top','none')
-jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Body').css('border-top','1px solid rgb(222,122,0)')
-jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Body').css('border-bottom','none')
+jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Body').css('border','1px solid rgb(222,122,0)')
   //also call the function on dynamic updates in addition to page load
   jQuery('.twitter-block').find('.twitter-timeline').contents().find('.timeline-TweetList').bind('DOMSubtreeModified propertychange', function() {
    customizeTweetMedia(this);
