2e8e45ca133121baa0a9ead88215ca736b38c2c1
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2020-11-27 09:53:47 +0100

    prettylink

--- static/twitter.js
+++ static/twitter.js
@@ -1,11 +1,11 @@
 jQuery('.twitter-feed').delegate('#twitter-widget-0','DOMSubtreeModified propertychange', function() {
-//function call to override the base twitter styles
 customizeTweetMedia();
 });
 
 var customizeTweetMedia = function() {
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('img.Avatar').css('display', 'none');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-avatar.Identity-avatar').remove();
+jQuery('.twitter-feed').find('.twitter-timeline').contents().find('a.PrettyLink').css('color','rgb(222,122,0)')
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-text').css('font-size', '16px');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Header').css('display', 'none');
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-LoadMore').css('display','none')
@@ -20,5 +20,4 @@ jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Twe
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList-tweet').css('border-top','1px solid rgb(222,122,0)')
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList-tweet:first-of-type').css('border-top','none')
 jQuery('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Body').css('border','1px solid rgb(222,122,0)')
-  //also call the function on dynamic updates in addition to page load
 }
